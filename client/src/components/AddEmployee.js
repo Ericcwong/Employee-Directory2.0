6b061ab125940cc001506f1c5a23e51c5ae63213
Eric Wong <ericwong12@live.com>
2020-04-20 21:53:39 -0600

    This should fix the problem for heroku

--- client/src/components/AddEmployee.js
+++ client/src/components/AddEmployee.js
@@ -43,7 +43,7 @@ export default class AddEmployee extends Component {
           };
         console.log(employee);
 
-        axios.post("/api/employees", employee)
+        axios.post("http://localhost:3001/api/employees", employee)
           .then(res => console.log(res.data));
 
     }
